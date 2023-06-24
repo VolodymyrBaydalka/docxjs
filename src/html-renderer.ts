eb48baa39e0d58239b0cac43e2899625e9d41a69
champkeh <champion.ge@gmail.com>
2023-06-24 20:53:56 +0800

    fix: Chinese numbering format mapping

--- src/html-renderer.ts
+++ src/html-renderer.ts
@@ -1190,6 +1190,7 @@ section.${c}>article { margin-bottom: auto; }
 			"upperLetter": "upper-alpha",
 			"lowerRoman": "lower-roman",
 			"upperRoman": "upper-roman",
+			"chineseCounting": "cjk-decimal",
 		};
 
 		return mapping[format] || format;
