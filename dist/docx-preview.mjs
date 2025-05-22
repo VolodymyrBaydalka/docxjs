968d35db62b17894aeb23ca1b4adf66c785b1a6e
Dan Schreiber <dmschreiber@gmail.com>
2025-05-22 16:17:05 -0400

    committing build resources
    As this appears to be what the other PRs have done

--- dist/docx-preview.mjs
+++ dist/docx-preview.mjs
@@ -99,22 +99,14 @@ function clamp(val, min, max) {
 }
 
 const ns$1 = {
-    wordml: "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
-    drawingml: "http://schemas.openxmlformats.org/drawingml/2006/main",
-    picture: "http://schemas.openxmlformats.org/drawingml/2006/picture",
-    compatibility: "http://schemas.openxmlformats.org/markup-compatibility/2006",
-    math: "http://schemas.openxmlformats.org/officeDocument/2006/math"
-};
+    wordml: "http://schemas.openxmlformats.org/wordprocessingml/2006/main"};
 const LengthUsage = {
     Dxa: { mul: 0.05, unit: "pt" },
     Emu: { mul: 1 / 12700, unit: "pt" },
     FontSize: { mul: 0.5, unit: "pt" },
     Border: { mul: 0.125, unit: "pt", min: 0.25, max: 12 },
     Point: { mul: 1, unit: "pt" },
-    Percent: { mul: 0.02, unit: "%" },
-    LineHeight: { mul: 1 / 240, unit: "" },
-    VmlEmu: { mul: 1 / 12700, unit: "" },
-};
+    Percent: { mul: 0.02, unit: "%" }};
 function convertLength(val, usage = LengthUsage.Dxa) {
     if (val == null || /.+(p[xt]|[%])$/.test(val)) {
         return val;
@@ -2638,7 +2630,7 @@ class values {
     }
     static valueOfBorder(c) {
         var type = globalXmlParser.attr(c, "val");
-        if (type == "nil")
+        if (type == "nil" || type == "none")
             return "none";
         var color = xmlUtil.colorAttr(c, "color");
         var size = globalXmlParser.lengthAttr(c, "sz", LengthUsage.Border);
